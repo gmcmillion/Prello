8298bb5e4d15749e84138e22aaf2a2e424616867
Gregg M <gmcmillion215@gmail.com>
2017-06-24 00:26:51 -0700

    modal fixes
    - Added ability to delete a card from within modal

--- board.js
+++ board.js
@@ -1,19 +1,6 @@
 //Global variable for keeping count of add buttons
 var numOfAddButtons = 2;    //initialize to 4 since we start with dummy data
 
-//Drop down menu for labels (DONE)
-function addLabel() {
-    document.getElementById("labelDropdown").classList.toggle("labelshow");
-}
-
-
-
-
-
-
-
-
-
 //Add a LIST to existing list
 var lastid = 1;
 addAnother = function() {
@@ -61,8 +48,8 @@ deleteList() = function(itemid) {
 
 
 //Initial Dummy cards
-var card1 = {name:"Card 1", description:"This is a my 1st card!"};
-var card2 = {name:"Card 2", description:"This is a my 2nd card!"};
+var card1 = {name:"Card 1", description:"This is a my 1st card!", label:["green"]};
+var card2 = {name:"Card 2", description:"This is a my 2nd card!", label:["red"]};
 
 //Array to hold cards
 var arrayOfCards = [];
@@ -85,6 +72,8 @@ function openModal(num) {
         
     //Populate modal with relevant info
     document.getElementById("description").innerHTML = arrayOfCards[num].description;
+    document.getElementById("card-name").innerHTML = arrayOfCards[num].name;
+    //document.getElementById("labels").innerHTML = arrayOfCards[num].label[0];
     
     //Display modal
     modal.style.display = "block";
@@ -105,6 +94,9 @@ window.onclick = function(event) {
 }
 
 
+
+
+
 //Show menu slider (Open & close functions)
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -113,8 +105,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+
+
+
 //Jquery
 var lol;
+var temp;           //Global variable to store which modal is opened
 function main() {
     
     //Function for boards drop down menu
@@ -122,7 +118,30 @@ function main() {
     $('#board-btn').on('click', function() {
         $(this).next().slideToggle(400);
     });
-
+    
+    //Delete-card-btn
+    $('#delete-card-btn').on('click', function(){
+        //Remove this card from data structure
+        arrayOfCards.splice(temp, 1);
+        
+        //Remove li element from ul (find the nth child)
+        $('.inner-list li:nth-child('+(temp+1)+')').remove();
+        
+        //Close modal
+        modal.style.display = "none";
+    });
+    
+    //Function for labels dropdown menu
+    $('#labeldropdown').hide();
+    $('#label-btn').on('click', function() {
+        $(this).next().toggle();
+    });
+    
+    //Function for adding colors of the label to card
+    $('#green').on('click', function() {
+        alert('green');
+    });
+    
     //Function for 'Show Menu" slide effect
     //$('#mySidenav').hide();
     $('#show-menu-btn').on('click', function() {
@@ -132,7 +151,8 @@ function main() {
     //When you click on any li within the inner list, do something
     $('#list').on('click', '.inner-list li', function() {
         //Get the index of the list that was clicked
-        var temp = $(this).index();
+        temp = $(this).index();
+
         //Open modal
         openModal(temp);
     });
@@ -144,7 +164,7 @@ function main() {
     });
     
     //When submit is click
-    $('#submitButton').on('click', function(e) {  
+    $('#submit-btn').on('click', function(e) {  
         e.preventDefault();                     //Prevent refresh    
         var post = $('#newCardInput').val();    //Get input value
         
@@ -169,16 +189,14 @@ function main() {
         $('.inner-list').append(litag);
         
         //Create new card object & add new card to array
-        var card = {name:null, description:$('#newCardInput').val()};
+        var card = {name:"Add Card Name...", description:$('#newCardInput').val()};
         arrayOfCards[arrayOfCards.length] = card;
         
         //Hide input after submit, and reset form
         $('#addCardDropdown').hide();
-        $('#submit-button-form')[0].reset();
+        $('#submit-btn-form')[0].reset();
     });
 }
 
 //Will call function main when HTML & CSS are done loading
 $(document).ready(main);
-
-
