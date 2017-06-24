d56143e8cd5b6700b10952226855a5585cfeedee
Gregg M <gmcmillion215@gmail.com>
2017-06-23 21:57:21 -0700

    Added Jquery functionality
    Replaced most javascript with jQuery

--- board.js
+++ board.js
@@ -1,64 +1,20 @@
 //Global variable for keeping count of add buttons
 var numOfAddButtons = 2;    //initialize to 4 since we start with dummy data
 
-//For left task bar drop down menu
-function dropDownFunction() {
-   document.getElementById("myDropdown").classList.toggle("show");
+//Drop down menu for labels (DONE)
+function addLabel() {
+    document.getElementById("labelDropdown").classList.toggle("labelshow");
 }
 
-//Add card drop down menu
-function addCardFunction() {
-  document.getElementById("addCardDropdown").classList.toggle("add-card-show");
-}
 
-//Modal for adding cards
-var modal = document.getElementById("myModal");
-var btn1 = document.getElementById("myBtn1");
-var btn2 = document.getElementById("myBtn2");
-var span = document.getElementsByClassName("close")[0];
 
-btn1.onclick = function() {
-    modal.style.display = "block";
-}
 
-btn2.onclick = function() {
-    modal.style.display = "block";
-}
 
-span.onclick = function() {
-    modal.style.display = "none";
-}
 
-window.onclick = function(event) {
-    if(event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
-//Show menu slider
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
 
-//Initial Dummy cards
-var card1 = {name:"Board 1", description:"This is a my 1st card!"};
-var card2 = {name:"Board 2", description:"This is a my 2nd card!"};
-
-//Array to hold cards
-var arrayOfCards = [];
-
-//Put dummy cards in Array
-arrayOfCards[0] = card1;
-arrayOfCards[1] = card2;
-
-document.getElementById("testCard1").innerHTML = arrayOfCards[0].description;
-document.getElementById("testCard2").innerHTML = arrayOfCards[1].description;
-
-//Add a list to existing list
+//Add a LIST to existing list
 var lastid = 1;
 addAnother = function() {
     //Create a add button and set attributes
@@ -82,7 +38,7 @@ addAnother = function() {
     var ul = document.getElementById("list");
     var li = document.createElement("li");
     var children = ul.children.length;
-    li.setAttribute("id", "element"+children);
+    //li.setAttribute("id", "element"+children);
     li.appendChild(btn2);
     li.appendChild(btn);
     ul.appendChild(li);
@@ -101,6 +57,9 @@ deleteList() = function(itemid) {
 }
 */
 
+
+
+
 //Initial Dummy cards
 var card1 = {name:"Card 1", description:"This is a my 1st card!"};
 var card2 = {name:"Card 2", description:"This is a my 2nd card!"};
@@ -116,50 +75,110 @@ arrayOfCards[1] = card2;
 document.getElementById("testCard1").innerHTML = arrayOfCards[0].description;
 document.getElementById("testCard2").innerHTML = arrayOfCards[1].description;
 
-//Get input from 
-var desc = document.querySelector('#newCardInput');
-var submitbtn = document.querySelector('#submitButton');
-var submitbtn_form = document.querySelector('#submit-button-form');
-
-//Function for adding a new card
-submitbtn_form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    //Toggle switch to close dropdown
-    addCardFunction();
-    
-    //Create new card object
-    var card = {name:null, description:null};
-    
-    //Put the input into the description
-    card.description = desc.value;
-    
-    //Push the card onto the array
-    arrayOfCards[arrayOfCards.length] = card;
-    
-    //Create a p tag, and insert its text
-    var p = document.createElement("P");
-    var tp = document.createTextNode(card.description);
-    //var tp = document.createTextNode("card.description");
-    p.appendChild(tp);
-    
-    //Create a button for the card
-    var btn = document.createElement("BUTTON");
-    btn.setAttribute("class", "cardBtn");
-    btn.appendChild(p);
-    
-    //Add new li for the new card
-    var ul = document.getElementById("inner-list");
-    var li = document.createElement("li");
-    li.appendChild(btn);
-    ul.appendChild(li);
-    
-    //Reset the form
-    submitbtn_form.reset();
-});
 
 
 
 
+
+//Function that opens modal
+function openModal(num) {
+        
+    //Populate modal with relevant info
+    document.getElementById("description").innerHTML = arrayOfCards[num].description;
+    
+    //Display modal
+    modal.style.display = "block";
+}
+
+//Close modal if x is clicked or off screen
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+//Show menu slider (Open & close functions)
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+//Jquery
+var lol;
+function main() {
+    
+    //Function for boards drop down menu
+    $('#board-btn-content').hide();
+    $('#board-btn').on('click', function() {
+        $(this).next().slideToggle(400);
+    });
+
+    //Function for 'Show Menu" slide effect
+    //$('#mySidenav').hide();
+    $('#show-menu-btn').on('click', function() {
+        openNav();
+    });
+        
+    //When you click on any li within the inner list, do something
+    $('#list').on('click', '.inner-list li', function() {
+        //Get the index of the list that was clicked
+        var temp = $(this).index();
+        //Open modal
+        openModal(temp);
+    });
+
+    //On click, show "Add a card" input box
+    $('#addCardDropdown').hide();
+    $('.addNewCardbtn').on('click', function() {
+        $(this).next().toggle();
+    });
+    
+    //When submit is click
+    $('#submitButton').on('click', function(e) {  
+        e.preventDefault();                     //Prevent refresh    
+        var post = $('#newCardInput').val();    //Get input value
+        
+        //Create new li element
+        //$('<li>').text(post).appendTo('.inner-list');
+        
+        //Create new p tag
+        var ptag = document.createElement("p");
+        ptag.innerHTML = post;
+        ptag.setAttribute
+
+        //Create new button that will store p tag 
+        var tempBtn = document.createElement("button");
+        tempBtn.setAttribute("class", "cardBtn");
+        tempBtn.append(ptag);
+
+        //Create new li tag that will store button
+        var litag = document.createElement("li");
+        litag.append(tempBtn);
+        
+        //Append litag to '.inner-list'
+        $('.inner-list').append(litag);
+        
+        //Create new card object & add new card to array
+        var card = {name:null, description:$('#newCardInput').val()};
+        arrayOfCards[arrayOfCards.length] = card;
+        
+        //Hide input after submit, and reset form
+        $('#addCardDropdown').hide();
+        $('#submit-button-form')[0].reset();
+    });
+}
+
+//Will call function main when HTML & CSS are done loading
+$(document).ready(main);
 
 
