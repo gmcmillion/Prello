d1d61cbbec53e74cc8b7d3b29a2e9c263ee3423d
Gregg M <gmcmillion215@gmail.com>
2017-07-17 13:41:36 -0700

    Final

---
+++ public/javascripts/resetpass.js
@@ -0,0 +1,16 @@
+function main() {
+    /*
+    $('#reset-btn').on('click', function() {
+        console.log('clicked');
+
+        var post_url = "./resetpass2";
+        
+        $.ajax({
+            url: post_url,
+            type: "GET"
+        });
+    });
+    */
+};
+
+$(document).ready(main);
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,18 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 var models = require('../models/allModels');
+var passwordHash = require('password-hash');
 
 //Render login page
 router.get('/', function(req, res) {
 	res.render('login.ejs');
 });
 
-/*
-//GET users listings
-router.get('/users', function(req, res, next) {
-	models.User.find(function (err, user){
-    	if (err) 
-      		console.log(err);
-    	res.json(user);
-  	});
-});
-*/
 
 router.get('/resetpass', function(req, res) {
-	res.render('forgotpass.ejs');
+	res.render('forgotpass.ejs', {error: ''});
 });	
-
-router.get('/resetpass2', function(req, res) {
-	res.render('reset-pass2.ejs');
-});	
-
 
 //Check if email exists
 router.post('/checkemail', function(req, res) {	
@@ -34,24 +21,85 @@ router.post('/checkemail', function(req, res) {
 	models.User.findOne({email: req.body.email}, function(err, user) {
 		if(!user) {
 			console.log('User does not exist!');
-			//res.render('forgotpass.ejs', {error: 'Invalid username'});
-			//res.redirect('/users');
+			res.render('forgotpass.ejs', {error: 'EMAIL DOES NOT EXIST!!'});
 		} else {
 			console.log('FOUND!');
 
+			//Hash the email
+			var hashedEmail = passwordHash.generate('user.email');
+
 			//Redirect to next page
+			res.redirect('/resetpass1/'+user._id+'/'+hashedEmail);
+			//res.render('reset-pass1.ejs');
+			//res.render('reset-pass1.ejs', {id: user._id, email: hashedEmail});
+		}
+	});
+});
+
+router.post('/resetpass2/:uid/:email', function(req, res) {
+
+	var tempurl = '/users/newpass/'+req.params.uid+"/"+req.params.email;
+	res.render('reset-pass2.ejs', {url: tempurl});
+});
+
+//Create new hashed password
+router.post('/newpass/:uid/:email', function(req, res) {
+	console.log('FINAL');
+	console.log(req.body);
+	
+	if(req.body.password !== req.body.confirmpassword) {
+		console.log('password mismatch');
+		res.redirect('/users');
+	}
+	else {
+		var hashedpass = passwordHash.generate('req.body.password');
+
+		models.User.findOneAndUpdate({"_id": mongoose.Types.ObjectId(req.params.uid)}, 
+		{ $set: {password: hashedpass} },
+		function(err, board) {		
+			if(err)
+				console.log(err);
+			else {
+				console.log('RESET');
+				res.redirect('/users');
+			}	
+		});
+	}
+
+});
+
+router.get('/resetpass1/:uid/:email', function(req, res) {
+	var tempurl = '/users/resetpass2/'+req.params.uid+"/"+req.params.email;
+	res.render('reset-pass1.ejs', {id: req.params.uid, email: req.params.email, url: tempurl});
+	/*
+	//Verify 
+	models.User.findOne({_id: req.params.uid}, function(err, user) {
+		if(!user) {
+			console.log('User does not exist!');
+			//res.render('forgotpass.ejs', {error: 'Invalid username'});
+			//res.redirect('/users');
+		} else {
+			console.log('FOUND IT!');
+			console.log('user email: '+user.email);
+			console.log('hashed email: '+req.params.email);
+			console.log(passwordHash.verify('user.email', req.params.email));
+			//console.log(passwordHash.verify('user.password', ));
 			res.render('reset-pass1.ejs');
 		}
 	});
+	*/
 });
 
 
 //To register a new user
 router.post('/reg', function(req, res) {	
+	//Hash Password
+	var hashedPassword = passwordHash.generate('req.body.password');
+
 	//Create user object
 	var user = new models.User({
 		username: req.body.username,
-		password: req.body.password,
+		password: hashedPassword,
 		email: req.body.email,
 		boards: []
 	});
